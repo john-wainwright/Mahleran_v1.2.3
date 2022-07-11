f3de7a545a6ab83b8ac53dfa2538148ffe5f6d2b
john-wainwright <john.wainwright@durham.ac.uk>
2022-07-11 16:03:29 +0100

    testing of updated compilers

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW_1-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_1-Windows
+CND_PLATFORM_Debug=MinGW64-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW64-Windows
 CND_ARTIFACT_NAME_Debug=mahleran_v1.2.3
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_1-Windows/mahleran_v1.2.3
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_1-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW64-Windows/mahleran_v1.2.3
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW64-Windows/package
 CND_PACKAGE_NAME_Debug=mahleranv1.2.3.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_1-Windows/package/mahleranv1.2.3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW64-Windows/package/mahleranv1.2.3.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW_1-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW_1-Windows
+CND_PLATFORM_Release=MinGW64-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW64-Windows
 CND_ARTIFACT_NAME_Release=mahleran_v1.2.3
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW_1-Windows/mahleran_v1.2.3
-CND_PACKAGE_DIR_Release=dist/Release/MinGW_1-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW64-Windows/mahleran_v1.2.3
+CND_PACKAGE_DIR_Release=dist/Release/MinGW64-Windows/package
 CND_PACKAGE_NAME_Release=mahleranv1.2.3.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW_1-Windows/package/mahleranv1.2.3.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW64-Windows/package/mahleranv1.2.3.tar
 #
 # include compiler specific variables
 #
